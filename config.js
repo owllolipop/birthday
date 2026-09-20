0fb07e27b4e1348a88cb26c44de2cef0988c38d0
owllolipop <manuellacintajesslyne@gmail.com>
2026-09-20 13:58:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.APP = {
   NAME: "yolabrentwife",
   PIN: "021010",
 
-  SUPABASE_URL: "https://dcvtbqzfzqzzzyeflpni.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://dcvtbqzfzqzzzyeflpni.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjdnRicXpmenF6enp5ZWZscG5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4NzMzMTQsImV4cCI6MjEwNTQ0OTMxNH0.aLlc3osD-IKorAQ_OyDNqV2mBzmMlGhZWlBV4TYeBW4",
 
   BUCKET: "wish-photos"
